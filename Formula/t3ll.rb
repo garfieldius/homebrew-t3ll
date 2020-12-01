eb129d7177f535162017d0f8a48b6f7c761711b2
Georg Großberger <contact@grossberger-ge.org>
2020-12-01 14:57:18 +0100

    Update to v0.6.0

--- Formula/t3ll.rb
+++ Formula/t3ll.rb
@@ -1,12 +1,12 @@
 
-# Generated on 2020.11.03 10:23:37
+# Generated on 2020.12.01 14:56:36
 # Do not edit manually
 
 class T3ll < Formula
   desc "Simple tool for editing TYPO3 localization files"
   homepage "https://github.com/garfieldius/t3ll/"
-  url "https://github.com/garfieldius/t3ll/archive/v0.5.5.tar.gz"
-  sha256 "0878064353249455df4460fc5eec9633a2a8fef7047009da2a42584475a5d0ad"
+  url "https://github.com/garfieldius/t3ll/archive/v0.6.0.tar.gz"
+  sha256 "04942c4b8a3387360976bdd353227aba366b856bee122a33d916c16249bf02e4"
 
   depends_on "go" => :build
   depends_on "node" => :build
@@ -14,10 +14,10 @@ class T3ll < Formula
   depends_on "yarn" => :build
 
   bottle do
-    root_url "https://github.com/garfieldius/t3ll/releases/download/v0.5.5/"
+    root_url "https://github.com/garfieldius/t3ll/releases/download/v0.6.0/"
     cellar :any_skip_relocation
-    sha256 "84a7a5c680351c1822eed1dd4e8b7628b0f8e5eeccb81aebbd495a14ce2ed96b" => :x86_64_linux
-    sha256 "a4e43a0bf7d077479efe07b1cfa07e406132c4cf8c36a8914c364ea68916e2fd" => :sierra
+    sha256 "aff578f9c27cfea12af00927bd56a910a1d6609c3bfbae1ae0072c50a24fb2f2" => :x86_64_linux
+    sha256 "04982b7750d0109dbddc5879deefea18514d75f5eb33e34d40f64ac81fe7ab13" => :sierra
   end
 
   def install
